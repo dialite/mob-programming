3e9373af4ae267e55647e9e225de91c4701e717d
dialite <raymondolisa01@gmail.com>
2023-02-11 11:41:38 +0100

    Final output for simple array sum

--- challenge.js
+++ challenge.js
@@ -31,6 +31,12 @@ function readLine() {
 
 function simpleArraySum(ar) {
   // Write your code here
+  let sum = 0;
+  for (let i = 0; i < ar.length; i++) {
+    sum += ar[i];
+  }
+
+  return sum;
 }
 
 function main() {
