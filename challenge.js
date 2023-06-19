6d36afd0a208845cda4a402a38fa1dc6a56f6849
dialite <raymondolisa01@gmail.com>
2023-06-19 12:45:49 +0100

    Create is prime function consideing is input and output

--- challenge.js
+++ challenge.js
@@ -5,3 +5,22 @@
 // Note: A prime number is a number greater than one(1) that is not a product of two(2) smaller natural numbers.
 // isPrime(5) = true (1*5 or 5*1)
 // isPrime(4) = false (1*4 or 2*2 or 4*1)
+
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// TIME COMPLEXITY
+// Big-O: O(n) - Linear time complexity
+
+console.log(isPrime(1)); // false
+console.log(isPrime(5)); // true
+console.log(isPrime(4)); // false
