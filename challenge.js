e53626fca8af337fc16027f61c09f753e08af0d7
dialite <raymondolisa01@gmail.com>
2023-06-19 13:44:53 +0100

    Power of two challenge with big o notation

--- challenge.js
+++ challenge.js
@@ -6,3 +6,31 @@
 // isPowerOfTwo(1) = true (2^0)
 // isPowerOfTwo(2) = true (2^1)
 // isPowerOfTwo(5) = false
+
+// PSEUDOCODE
+// N = 8
+
+// 8/2 = 4 (remainder 0)
+// 4/2 = 2 (remainder 0)
+// 2/2 = 1 (remainder 0)
+// If remainder is 0 and 'n' comes down to 1 eventually, n is a power of two
+
+function isPowerOfTwo(n) {
+  if (n < 1) {
+    return false;
+  }
+  while (n > 1) {
+    if (n % 2 !== 0) {
+      return false;
+    }
+    n = n / 2;
+  }
+  return true;
+}
+
+// TIME COMPLEXITY
+// Big-O : O(logn) - Size reduces by half every iteration
+
+console.log(isPowerOfTwo(1)); // true
+console.log(isPowerOfTwo(2)); // true
+console.log(isPowerOfTwo(5)); // false
