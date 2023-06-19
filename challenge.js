676f681538460fcfc730ccb7732ed0eff47b8122
dialite <raymondolisa01@gmail.com>
2023-06-19 12:16:48 +0100

    Initialization for prime numbers

--- challenge.js
+++ challenge.js
@@ -1,23 +1,7 @@
-// FACTORIAL OF A NUMBER
+// PRIME NUMBER
 
-// QUESTION:       Given an integer 'n', find the factorial of that integer
+// QUESTION:       Given a natural number 'n', determine if the number is a prime or not
 
-// Note: Factorial of a non-negative integer "n", denoted n!, is the product of all positive integers less than or equal to 'n'.
-// Factorial zero(0) is 1 and one(1) is also 1.
-//       factorial(4) = 4*3*2*1 = 24
-// factorial(5) = 5*4*3*2*1 = 120
-
-function factorial(n) {
-  let result = 1;
-  for (let i = 2; i <= n; i++) {
-    result = result * i;
-  }
-  return result;
-}
-
-// TIME COMPLEXITY
-// Big-O = O(n) - Linear Time Complexity
-
-console.log(factorial(0)); // 1
-console.log(factorial(1)); // 1
-console.log(factorial(5)); // 120
+// Note: A prime number is a number greater than one(1) that is not a product of two(2) smaller natural numbers.
+// isPrime(5) = true (1*5 or 5*1)
+// isPrime(4) = false (1*4 or 2*2 or 4*1)
