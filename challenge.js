af745faaa140529f7c8577347d0771845df1e8dc
dialite <raymondolisa01@gmail.com>
2023-06-19 12:10:16 +0100

    Create function to accept input process it and give output for factorial of an number

--- challenge.js
+++ challenge.js
@@ -1,15 +1,23 @@
-// FIBONACCI SEQUENCE
+// FACTORIAL OF A NUMBER
 
 // QUESTION:       Given an integer 'n', find the factorial of that integer
 
-function fibonacci(n) {
-  const fib = [0, 1];
-  for (let i = 2; i < n; i++) {
-    fib[i] = fib[i - 1] + fib[i - 2];
+// Note: Factorial of a non-negative integer "n", denoted n!, is the product of all positive integers less than or equal to 'n'.
+// Factorial zero(0) is 1 and one(1) is also 1.
+//       factorial(4) = 4*3*2*1 = 24
+// factorial(5) = 5*4*3*2*1 = 120
+
+function factorial(n) {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result = result * i;
   }
-  return fib;
+  return result;
 }
 
-console.log(fibonacci(2)); //[0,1]
-console.log(fibonacci(3)); //[0,1,1]
-console.log(fibonacci(7)); //[0,1,1,2,3,5,8]
+// TIME COMPLEXITY
+// Big-O = O(n) - Linear Time Complexity
+
+console.log(factorial(0)); // 1
+console.log(factorial(1)); // 1
+console.log(factorial(5)); // 120
