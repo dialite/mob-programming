97d7ee8fb84f343435b601e2023491a0ef6d428f
dialite <raymondolisa01@gmail.com>
2023-06-19 13:02:00 +0100

    Solve is prime number challenge using a more efficient way of focusing on only the square root of a number

--- challenge.js
+++ challenge.js
@@ -6,11 +6,28 @@
 // isPrime(5) = true (1*5 or 5*1)
 // isPrime(4) = false (1*4 or 2*2 or 4*1)
 
+// function isPrime(n) {
+//   if (n < 2) {
+//     return false;
+//   }
+//   for (let i = 2; i < n; i++) {
+//     if (n % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// // TIME COMPLEXITY
+// // Big-O: O(n) - Linear time complexity
+
+// FOR MORE EFFICIENCY ABOUT FOCUSING ON ONLY THE NUMBERS LESS THAN THE SQUARE ROOT OF 'n' The following can be done
+
 function isPrime(n) {
   if (n < 2) {
     return false;
   }
-  for (let i = 2; i < n; i++) {
+  for (let i = 2; i <= Math.sqrt(n); i++) {
     if (n % i === 0) {
       return false;
     }
@@ -19,7 +36,7 @@ function isPrime(n) {
 }
 
 // TIME COMPLEXITY
-// Big-O: O(n) - Linear time complexity
+// Big-O: O(sqrt(n)) - O of square root of n time complexity
 
 console.log(isPrime(1)); // false
 console.log(isPrime(5)); // true
