475981d215650c5363147e494691744f745dfaa0
dialite <raymondolisa01@gmail.com>
2023-06-19 11:38:29 +0100

    Write code on Fibonacci sequence

--- challenge.js
+++ challenge.js
@@ -1,57 +1,13 @@
-"use strict";
+// FIBONACCI SEQUENCE
 
-const fs = require("fs");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
-/*
- * Complete the 'simpleArraySum' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts INTEGER_ARRAY ar as parameter.
- */
-
-function simpleArraySum(ar) {
-  // Write your code here
-  let sum = 0;
-  for (let i = 0; i < ar.length; i++) {
-    sum += ar[i];
+function fibonacci(n) {
+  const fib = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
   }
-
-  return sum;
+  return fib;
 }
 
-function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-  const arCount = parseInt(readLine().trim(), 10);
-
-  const ar = readLine()
-    .replace(/\s+$/g, "")
-    .split(" ")
-    .map((arTemp) => parseInt(arTemp, 10));
-
-  const result = simpleArraySum(ar);
-
-  ws.write(result + "\n");
-
-  ws.end();
-}
+console.log(fibonacci(2)); //[0,1]
+console.log(fibonacci(3)); //[0,1,1]
+console.log(fibonacci(7)); //[0,1,1,2,3,5,8]
