c27cd82f2469c2cfe6998846c69a5583d2aec80f
dialite <raymondolisa01@gmail.com>
2023-06-20 14:40:59 +0100

    Solve linear search challenge

--- challenge.js
+++ challenge.js
@@ -1,6 +1,6 @@
-// RECURSIVE FACTORIAL OF A NUMBER
+// LINEAR SEARCH
 
-// QUESTION:       Given a integer 'n', find the factorial of that integer.
+// QUESTION:       Given an array of 'n' and a target element 't', find the index of 't' in the array. Return -1 if the target element is not found.
 
 // Note: A Fibonacci sequence is a sequence in which each number is the sum of the two preceding ones.
 // recursiveFactorial(0) = 1
@@ -8,30 +8,28 @@
 // recursiveFibonacci(4) = 24
 
 // PSEUDOCODE
-// n = 5  =  5*4*3*2*1  = 5 * 4!   =   120
-// n = 4  =  4*3*2*1  =  4 * 3! =  24
-// n = 3  =  3*2*1  =  3 * 2!  =  6                                 n!= n*(n-1)!
-// n = 2  =  2*1  =  2 * 1!  =  2
-// n = 1  =  1  =  1 * 0! = 1
-// n = 0  =  1
+// Start at the first element of the array and move towards the last.
+// At each element though, check if the element is equal to the target element.
+// If element found, return the index of the element
+// If element not found, return -1
 
 // THEREFORE
-// Fn = F(n - 1) + F(n - 2);
-// F(0) = 0 and F(1) = 1    ---- Base case
-// F(2) = F(1) + F(0)
+// arr = [-5, 2, 10, 4, 6], t = 10  =  should return 2
+// arr = [-5, 2, 10, 4, 6], t = 6  =  should return 4
+// arr = [-5, 2, 10, 4, 6], t = 20  =  should return -1
 
-function recursiveFactorial(n) {
-  // n! = n * (n - 1)!
-  if (n === 0) {
-    //Base case
-    return 1;
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
   }
-  return n * recursiveFactorial(n - 1);
+  return -1;
 }
 
 // TIME COMPLEXITY
-// Big-O : O(n) - It is Linear because the higher n is, the more the number of instructions executed of n
+// Big-O : O(n) - It is Linear time complexity because it has one loop
 
-console.log(recursiveFactorial(0)); // 1
-console.log(recursiveFactorial(3)); // 6
-console.log(recursiveFactorial(5)); // 120
+console.log(linearSearch([-5, 2, 10, 4, 6], 10)); // 2
+console.log(linearSearch([-5, 2, 10, 4, 6], 6)); // 4
+console.log(linearSearch([-5, 2, 10, 4, 6], 20)); // -1
