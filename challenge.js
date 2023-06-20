1a5c5be6ad85a3735571a8f1d6b175e253de2a84
dialite <raymondolisa01@gmail.com>
2023-06-20 16:37:39 +0100

    Solve challenge by sorting array with bubble sort

--- challenge.js
+++ challenge.js
@@ -1,8 +1,11 @@
-// RECURSIVE BINARY SEARCH
+// BUBBLE SORT
 
-// QUESTION:       Given a sorted array of 'n' elements and a target element 't', find the index of 't' in the array. Return -1 if the target element is not found.
+// QUESTION:       Given an array of integers, sort the array.
 
-// Note: Binary search only works for sorted arrays. You are sort then apply search for unsorted arrys.
+// Note: In bubble sort, you compare adjacent elements in the array and swap the positions if they are not in the intended order.
+// It checks after each iteration if there was a swap. If yes, it will then go over the array until an iteration when there is no swap.
+// The highest number is takien to the top, that is why it is called bubble sort.
+// Bubble sort is a poor sorting method and only used during interviews and few other reasons.
 
 // PSEUDOCODE
 
@@ -10,39 +13,29 @@
 // If the array is empty reurn -1 as the element cannot be found
 // If the array hass ElementInternals, find the middle element in the array. If the target is equal to the middle element, return the middle element index.
 
-// SOLUTION TO THE PROBLEM
-// If target is less than the middle Element, binary search left half of the array.
-// If target is greater than the middle Element, binary search right half of the array.
-
 // THEREFORE
 // sortedArr = [-5, 2, 4, 6, 10], t = 10  =  should return 4
 // sortedArr = [-5, 2, 4, 6, 10], t = 6  =  should return 3
 // sortedArr = [-5, 2, 4, 6, 10], t = 20  =  should return -1
 
-function recursiveBinarySearch(arr, target) {
-  return search(arr, target, 0, arr.length - 1);
-}
-
-function search(arr, target, leftIndex, rightIndex) {
-  if (leftIndex > rightIndex) {
-    return -1;
-  }
-
-  let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
-  if (target === arr[middleIndex]) {
-    return middleIndex;
-  }
-
-  if (target < arr[middleIndex]) {
-    return search(arr, target, leftIndex, middleIndex - 1);
-  } else {
-    return search(arr, target, middleIndex + 1, rightIndex);
-  }
+function bubbleSort(arr) {
+  let sorted;
+  do {
+    sorted = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        sorted = true;
+      }
+    }
+  } while (sorted);
 }
 
 // TIME COMPLEXITY
 // Big-O : O(logn) - It is Logrithmic time complexity because the loop halves after each iteration
 
-console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 10)); // 4
-console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 6)); // 3
-console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 20)); // -1
+const arr = [8, 20, -2, 4, -6];
+bubbleSort(arr);
+console.log(arr); // [-6, -2, 4, 8, 20]
