14431ed48dd7550aa5e90f6e1a985d85ab445d83
dialite <raymondolisa01@gmail.com>
2023-06-21 16:21:07 +0100

    Solve cartesian product challenge

--- challenge.js
+++ challenge.js
@@ -1,4 +1,4 @@
-// MERGE SORT
+// CARTESIAN PRODUCT
 
 // QUESTION:       Given two finite non-empty sets, find the Cartesian Product.
 
@@ -9,33 +9,19 @@
 
 // PSEUDOCODE
 
-function mergeSort(arr) {
-  if (arr.length < 2) {
-    return arr;
-  }
+function cartesianProduct(arr1, arr2) {
+  const result = [];
 
-  //Reduce length of array to single arrays
-  const mid = Math.floor(arr.length / 2);
-  const leftArray = arr.slice(0, mid);
-  const rightArray = arr.slice(mid);
-  return merge(mergeSort(leftArray), mergeSort(rightArray));
-}
-
-// Removes the smallest first element between the two arrays
-function merge(leftArray, rightArray) {
-  const sortedArr = [];
-  while (leftArray.length && rightArray.length) {
-    if (leftArray[0] <= rightArray[0]) {
-      sortedArr.push(leftArray.shift());
-    } else {
-      sortedArr.push(rightArray.shift());
+  for (let a = 0; a < arr1.length; a++) {
+    for (let b = 0; b < arr2.length; b++) {
+      result.push([arr1[a], arr2[b]]);
     }
   }
-  return [...sortedArr, ...leftArray, ...rightArray];
+  return result;
 }
-
 // TIME COMPLEXITY
-// WORST CASE  :   O(nlogn)  -  There is a loop (n), and also the return is an array that is divided into smaller arrays (logn)
+// WORST CASE  :   O(m*n)  -  Because the length of the arrays can be different
 
-const arr = [8, 20, -2, 4, -6];
-console.log(mergeSort(arr)); // [-6, -2, 4, 8, 20]
+const arr1 = [1, 2];
+const arr2 = [3, 4, 5];
+console.log(cartesianProduct(arr1, arr2)); // [-6, -2, 4, 8, 20]
