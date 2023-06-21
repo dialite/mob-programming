c2e7d493afae56ec54d2dc1a37eb2a7b1663b4f3
dialite <raymondolisa01@gmail.com>
2023-06-21 13:37:23 +0100

    Solve quick sort array

--- challenge.js
+++ challenge.js
@@ -1,41 +1,47 @@
-// INSERTION SORT
+// QUICK SORT
 
 // QUESTION:       Given an array of integers, sort the array.
 
-// Note: In insertion sort, virtually split the array into a sorted and an unsorted part.
-// Assume that the first element is already sorted and remaining elements are unsorted.
-// Select an unsorted element and compare with all elements in the sorted part.
-// If the elements in the sorted part is smaller than the selected element, proceed to the next element in the unsorted part.
-// Else, shift larger elements in the sorted part towards the right.
-// Insert the selected element at the right index.
-// Repeat till all the unsorted elements are placed in the right order.
+// Note:
+// 1) Identify the pivot element in the array.
+// a) Pick first element as pivot
+// b) Pick last element as pivot (Our approach)
+// c) Pick a random element as pivot
+// d) Pick median as pivot
+
+// 2) Put everything that's smaller than the pivot into a 'left' array and everything that's greater than the pivot into a 'right' array.
+
+// 3) Repeat the processs for the individual 'left' and 'right' arrays till you have an array of length 1 which is sorted by definition
+
+// 4) Repeatedly concatenate the left Array, pivot and right array till one sorted array remains
 
 // PSEUDOCODE
-
-// BASE CASE
-// If the array is empty reurn -1 as the element cannot be found
-// If the array hass ElementInternals, find the middle element in the array. If the target is equal to the middle element, return the middle element index.
 
 // THEREFORE
 // sortedArr = [-5, 2, 4, 6, 10], t = 10  =  should return 4
 // sortedArr = [-5, 2, 4, 6, 10], t = 6  =  should return 3
 // sortedArr = [-5, 2, 4, 6, 10], t = 20  =  should return -1
 
-function insertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    let numberToInsert = arr[i];
-    let j = i - 1; //'j' is the index of the sorted element
-    while (j >= 0 && arr[j] > numberToInsert) {
-      arr[j + 1] = arr[j];
-      j--;
-    }
-    arr[j + 1] = numberToInsert;
+function quickSort(arr) {
+  if (arr.length < 2) {
+    return arr;
   }
+  let pivot = arr[arr.length - 1];
+  let left = [];
+  let right = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
 // TIME COMPLEXITY
-// Big-O : O(n^2) - It is Quadratic time complexity because there are two loops
+// WORST CASE :   Big-O : O(n^2) - It is Quadratic time complexity because an already sorted array is split into and empty array then a full array
+// AVERAGE CASE  :   O(nlogn)  -  There is a loop (n), and also the return is an array that is divided into smaller arrays (logn)
 
 const arr = [8, 20, -2, 4, -6];
-insertionSort(arr);
-console.log(arr); // [-6, -2, 4, 8, 20]
+console.log(quickSort(arr)); // [-6, -2, 4, 8, 20]
