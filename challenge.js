55ba3b4ef3ea102d86cdbe678fcfc5220b683ad1
dialite <raymondolisa01@gmail.com>
2023-06-21 17:55:42 +0100

    Solve climbing stairs challenge

--- challenge.js
+++ challenge.js
@@ -1,27 +1,35 @@
-// CARTESIAN PRODUCT
+// CLIMBING STAIRCASE
 
-// QUESTION:       Given two finite non-empty sets, find the Cartesian Product.
+// QUESTION:       Given a staircase of 'n' steps, count the number of distinct ways to climb to the top. You can either climb 1 step or 2 steps at a time
 
 // NOTE:
 
-// In mathematics, specifically set theory, the Cartesian product of two sets of A and B, denoted A x B,
-// is the set of all ordered pairs (a, b) where a is in A and b is in B
+// n=1, climbingStaircase(1) = 1                   | (1)
+// n=1, climbingStaircase(2) = 2                   | (1, 1) and (2)
+// n=1, climbingStaircase(3) = 3                   | (1,1,1) (1,2) and (2,1)
+// n=1, climbingStaircase(5) = 5                   | (1,1,1,1) (1,1,2) (1,2,1) (2,1,1) and (2,2)
 
 // PSEUDOCODE
+// At any given TimeRanges, you can climb either 1 step or 2 steps
+// If you have to climb to step 'n', we can only climb from step 'n-1' or 'n-2'
+// Calculate the ways we can climb to 'n-1' and 'n-2' steps and add the two
 
-function cartesianProduct(arr1, arr2) {
-  const result = [];
+// climbingStaircase(n) = climbingStaircase(n-1) + climbingStaircase(n-2)
 
-  for (let a = 0; a < arr1.length; a++) {
-    for (let b = 0; b < arr2.length; b++) {
-      result.push([arr1[a], arr2[b]]);
-    }
+function climbingStaircase(n) {
+  const noOfWays = [1, 2];
+
+  for (let i = 2; i <= n; i++) {
+    noOfWays[i] = noOfWays[i - 1] + noOfWays[i - 2];
   }
-  return result;
+  return noOfWays[n - 1];
 }
 // TIME COMPLEXITY
-// WORST CASE  :   O(m*n)  -  Because the length of the arrays can be different
+// WORST CASE  :   O(n)  - Linear time complexity
 
-const arr1 = [1, 2];
-const arr2 = [3, 4, 5];
-console.log(cartesianProduct(arr1, arr2)); // [-6, -2, 4, 8, 20]
+console.log(climbingStaircase(1));
+console.log(climbingStaircase(2));
+console.log(climbingStaircase(3));
+console.log(climbingStaircase(4));
+console.log(climbingStaircase(5));
+console.log(climbingStaircase(6));
