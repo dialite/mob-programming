ae380ea032f5db1d7c4a19a2025c88df7f1091ee
dialite <raymondolisa01@gmail.com>
2023-06-21 15:37:49 +0100

    Solve merge sort challenge

--- challenge.js
+++ challenge.js
@@ -1,47 +1,42 @@
-// QUICK SORT
+// MERGE SORT
 
 // QUESTION:       Given an array of integers, sort the array.
 
 // Note:
-// 1) Identify the pivot element in the array.
-// a) Pick first element as pivot
-// b) Pick last element as pivot (Our approach)
-// c) Pick a random element as pivot
-// d) Pick median as pivot
 
-// 2) Put everything that's smaller than the pivot into a 'left' array and everything that's greater than the pivot into a 'right' array.
+// 1) Divide the array into sub arrays, each containing only one element (An array with one element is considered sorted).
 
-// 3) Repeat the processs for the individual 'left' and 'right' arrays till you have an array of length 1 which is sorted by definition
-
-// 4) Repeatedly concatenate the left Array, pivot and right array till one sorted array remains
+// 2) Repeatedly merge the sub arrays to produce new sorted sub arrays until there is only one sub array remaining. That will be the sorted array.
 
 // PSEUDOCODE
 
-// THEREFORE
-// sortedArr = [-5, 2, 4, 6, 10], t = 10  =  should return 4
-// sortedArr = [-5, 2, 4, 6, 10], t = 6  =  should return 3
-// sortedArr = [-5, 2, 4, 6, 10], t = 20  =  should return -1
-
-function quickSort(arr) {
+function mergeSort(arr) {
   if (arr.length < 2) {
     return arr;
   }
-  let pivot = arr[arr.length - 1];
-  let left = [];
-  let right = [];
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] < pivot) {
-      left.push(arr[i]);
+
+  //Reduce length of array to single arrays
+  const mid = Math.floor(arr.length / 2);
+  const leftArray = arr.slice(0, mid);
+  const rightArray = arr.slice(mid);
+  return merge(mergeSort(leftArray), mergeSort(rightArray));
+}
+
+// Removes the smallest first element between the two arrays
+function merge(leftArray, rightArray) {
+  const sortedArr = [];
+  while (leftArray.length && rightArray.length) {
+    if (leftArray[0] <= rightArray[0]) {
+      sortedArr.push(leftArray.shift());
     } else {
-      right.push(arr[i]);
+      sortedArr.push(rightArray.shift());
     }
   }
-  return [...quickSort(left), pivot, ...quickSort(right)];
+  return [...sortedArr, ...leftArray, ...rightArray];
 }
 
 // TIME COMPLEXITY
-// WORST CASE :   Big-O : O(n^2) - It is Quadratic time complexity because an already sorted array is split into and empty array then a full array
-// AVERAGE CASE  :   O(nlogn)  -  There is a loop (n), and also the return is an array that is divided into smaller arrays (logn)
+// WORST CASE  :   O(nlogn)  -  There is a loop (n), and also the return is an array that is divided into smaller arrays (logn)
 
 const arr = [8, 20, -2, 4, -6];
-console.log(quickSort(arr)); // [-6, -2, 4, 8, 20]
+console.log(mergeSort(arr)); // [-6, -2, 4, 8, 20]
