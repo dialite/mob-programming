82726b3e30a63996c4062d918084bb61acd291e8
dialite <raymondolisa01@gmail.com>
2023-06-23 13:46:27 +0100

    Initialize linked list and node

--- challenge.js
+++ challenge.js
@@ -1,28 +1,43 @@
-class CircularQueue {
-  constructor(capacity) {
-    this.items = new Array(capacity);
-    this.capacity = capacity;
-    this.currentLength = 0;
-    this.front = -1;
-    this.rear = -1;
-  }
+// LINKED LIST
 
-  isFull() {
-    return this.currentLength === this.capacity;
+// A linked list is a linear data structure that includes a series of connected nodes
+
+// Each node consists of a data value and a pointer that points to the next node
+
+// The list elements can be easily inserted or removed without reallocation or reorganization of the entire structure
+
+// Random access of elements is not feasible and accessing an element has a linear time complexity
+
+// The linked list data structure supports three main operations
+
+//   - Insertion : to add an element at the beginning, end or at a given index in the list
+//   - Deletion : To remove an item given its index or value
+//   - Search : To find an element given its value
+
+// E.g. Image viewer
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class linkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
   }
 
   isEmpty() {
-    return this.currentLength === 0;
+    return this.size === 0;
   }
 
-  enqueue(element) {
-    if (!this.isFull) {
-      this.rear = this.rear + 1;
-      this.items[this.rear] = element;
-      this.currentLength += 1;
-      if (this.front === -1) {
-        this.front = this.rear;
-      }
-    }
+  getSize() {
+    return this.size;
   }
 }
+
+const list = new linkedList();
+console.log("List is empty? ", list.isEmpty());
+console.log("List size ", list.getSize());
